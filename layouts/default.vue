184af6b28a7d1d6d2f0cbd3a87fc0d35f9333974
Amal Mohan K <amalmohan542@gmail.com>
2025-01-02 17:02:45 +0100

    Bugfix : Infinite scroll loader hidden behind footer minor bug fix.

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@ import Header from "@/components/Header.vue";
 <template>
   <div>
     <Header/>
-    <div class="container mx-auto bg-white p-4 xl:px-32">
+    <div class="container mx-auto bg-white p-4 xl:px-32 mb-8">
       <slot />
     </div>
     <Footer/>
