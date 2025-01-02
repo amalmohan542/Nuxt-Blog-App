9bfde2baaf7c89f5a41acf35405c011189d88aff
Amal Mohan K <amalmohan542@gmail.com>
2025-01-02 06:56:14 +0100

    Dev : Added multi lang support for contact page.

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,29 +1,26 @@
 <template>
-  <section
-    class="bg-white shadow-[0px_3px_10px_-1px_rgba(59,_130,_246,_0.5)] rounded-md"
-  >
-    <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+  <section class="w-full flex justify-center items-center">
+    <div class="w-3/4 *:py-2 lg:py-16 px-4 mx-auto max-w-screen-md">
       <h2
-        class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900"
+        class="mb-2 text-4xl tracking-tight font-extrabold text-center text-gray-900"
       >
-        Contact Us
+        {{ $t("contactUs") }}
       </h2>
-      <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 sm:text-xl">
-        Got a technical issue? Want to send feedback about a beta feature? Need
-        details about our Business plan? Let us know.
+      <p class="mb-2 lg:mb-4 font-light text-center text-gray-500 sm:text-xl">
+        {{ $t("contactUsDescription") }}
       </p>
       <form action="#" class="space-y-8">
         <div>
           <label
             for="email"
             class="block mb-2 text-sm font-medium text-gray-900"
-            >Your email</label
+            >{{ $t("yourEmail") }}</label
           >
           <input
             type="email"
             id="email"
             class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5"
-            placeholder="name@gmail.com"
+            :placeholder="$t('enterYourEmail')"
             required
           />
         </div>
@@ -31,13 +28,13 @@
           <label
             for="subject"
             class="block mb-2 text-sm font-medium text-gray-900"
-            >Subject</label
+            >{{ $t("subject") }}</label
           >
           <input
             type="text"
             id="subject"
             class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500"
-            placeholder="Let us know how we can help you"
+            :placeholder="$t('subjectPlaceholder')"
             required
           />
         </div>
@@ -45,21 +42,23 @@
           <label
             for="message"
             class="block mb-2 text-sm font-medium text-gray-900"
-            >Your message</label
+            >{{ $t("yourMessage") }}</label
           >
           <textarea
             id="message"
             rows="6"
             class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500"
-            placeholder="Leave a comment..."
+            :placeholder="$t('yourMessagePlaceholder')"
           ></textarea>
         </div>
-        <button
-          type="submit"
-          class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-gray-900 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300"
-        >
-          Send message
-        </button>
+        <div class="flex justify-center items-center">
+          <button
+            type="submit"
+            class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-gray-900 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300"
+          >
+            {{ $t("sendMessage") }}
+          </button>
+        </div>
       </form>
     </div>
   </section>
